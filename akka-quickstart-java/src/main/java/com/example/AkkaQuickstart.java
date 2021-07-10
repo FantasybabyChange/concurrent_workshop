22b62e8b505e43438c1bca9f1f49ab4ecebd623d
fantasybaby <625353861@qq.com>
2021-07-10 17:57:54 +0800

    add comments

--- akka-quickstart-java/src/main/java/com/example/AkkaQuickstart.java
+++ akka-quickstart-java/src/main/java/com/example/AkkaQuickstart.java
@@ -5,6 +5,13 @@ import akka.actor.typed.ActorSystem;
 import java.io.IOException;
 
 public class AkkaQuickstart {
+    /**
+     * The AkkaQuickstart object in Hello World creates ActorSystem with a guardian.
+     * The guardian is the top level actor that bootstraps your application.
+     * The guardian is typically defined with Behaviors.setup that contains the initial bootstrap.
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         //#actor-system
         final ActorSystem<GreeterMain.SayHello> greeterMain = ActorSystem.create(GreeterMain.create(), "helloakka");
